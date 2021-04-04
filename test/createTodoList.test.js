b7fe3706ee1008cf608302e4ce1106b1d42c5ee1
System Administrator <root@MacBook-Pro.local>
2021-04-04 16:12:55 -0300

    implementing test: update a task

--- test/createTodoList.test.js
+++ test/createTodoList.test.js
@@ -4,11 +4,7 @@ const { MongoClient } = require('mongodb');
 const mongoDbUrl = 'mongodb+srv://strsk:eGrsz2N6qvhPSV5P@cluster0.m9kzj.mongodb.net/test?authSource=admin&replicaSet=atlas-kq29l2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 const url = 'http://localhost:3000';
 
-const taskList = [
-  { task: 'read a book', status: 'pendent' },
-  { task: 'go to gym', status: 'pendent' },
-  { task: 'clean my room', status: 'pendent' },
-];
+const taskList = [{ task: 'read a book', status: 'pendent' }];
 
 const invalidEntries = 'All fields must be filled.'
 const conflict = 'Task already exists.'
@@ -34,28 +30,28 @@ afterAll(async () => {
 });
 
 describe('It is possible to create a new todo list', () => {
-  it('You cannot create a new to do list without the "tasks" field', async () => {
+  it('You cannot create a new task without the "tasks" field', async () => {
     await frisby
       .post(`${url}/todo`, { status: 'pendent' })
       .expect('status', 400)
       .then((response) => expect(response.json.message).toBe(invalidEntries))
   });
 
-  it('You cannot create a new to do list without the "status" field', async () => {
+  it('You cannot create a new task without the "status" field', async () => {
     await frisby
       .post(`${url}/todo`, { task: 'read a book' })
       .expect('status', 400)
       .then((response) => expect(response.json.message).toBe(invalidEntries))
   });
 
-  it('You cannot create a new to do list with the "task" field empty', async () => {
+  it('You cannot create a new task with the "task" field empty', async () => {
     await frisby
       .post(`${url}/todo`, { task: '', status: 'pendent' })
       .expect('status', 400)
       .then((response) => expect(response.json.message).toBe(invalidEntries))
   });
 
-  it('You cannot create a new to do list with the "status" field empty', async () => {
+  it('You cannot create a new task with the "status" field empty', async () => {
     await frisby
       .post(`${url}/todo`, { task: 'read a book', status: '' })
       .expect('status', 400)
@@ -68,29 +64,15 @@ describe('It is possible to create a new todo list', () => {
       .expect('status', 409)
       .then((response) => expect(response.json.message).toBe(conflict))
   });
-  
-  // it('You can create a new to do list with more than 1 task', async () => {
-  //   await frisby
-  //     .post(`${url}/todo`, taskList)
-  //     .expect('status', 201)
-  //     .then((response) => {
-  //       let body = response.body;
-  //       body = [JSON.parse(body)]
 
-  //       const firstTask = body[0].task;
-  //       const firstStatus = body[0].status;
-  //       const secondTask = body[1].task;
-  //       const secondStatus = body[1].status;
-  //       const thirdTask = body[2].task;
-  //       const thirdStatus = body[2].status;
-
-  //       expect(firstTask).toEqual(taskList[0].task)
-  //       expect(firstStatus).toEqual(taskList[0].status)
-  //       expect(secondTask).toEqual(taskList[1].task)
-  //       expect(secondStatus).toEqual(taskList[1].status)
-  //       expect(thirdTask).toEqual(taskList[2].task)
-  //       expect(thirdStatus).toEqual(taskList[2].status)
-  //     })
-  // });
-
+  it('You can create a new task successfully', async () => {
+    await frisby
+      .post(`${url}/todo`, { task: 'clean the kitchen', status: 'pendent' })
+      .expect('status', 201)
+      .then((response) => {
+        const { task, status } = response.json;
+        expect(task).toEqual('clean the kitchen');
+        expect(status).toEqual('pendent');
+      })
+  });
 });
---
+++ test/updateTodoList.test.js
@@ -0,0 +1,104 @@
+const frisby = require('frisby');
+const { MongoClient } = require('mongodb');
+
+const mongoDbUrl = 'mongodb+srv://strsk:eGrsz2N6qvhPSV5P@cluster0.m9kzj.mongodb.net/test?authSource=admin&replicaSet=atlas-kq29l2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+const url = 'http://localhost:3000';
+
+const invalidEntries = 'All fields must be filled.'
+const notFound = 'Task not found';
+
+let connection;
+let todo_list;
+let id;
+
+beforeAll(async () => {
+  connection = await MongoClient.connect(mongoDbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  todo_list = connection.db('todoList').collection('tasks');
+});
+
+beforeEach(async () => {
+  await todo_list.deleteMany({});
+  await todo_list.insertMany(taskList);
+});
+
+afterAll(async () => {
+  await connection.close();
+});
+
+describe('It is possible to update a task and it status', () => {
+
+  it('You cannot update a task that does not exists', () => {
+    await frisby
+      .put(`${url}/todo/1234563`, { task: 'clean the kitchen', status: 'pendent' })
+      .expect('status', 409)
+      .then((response) => response.json.message).toBe(notFound);
+  });
+
+  it('You cannot update a task without the "tasks" field', async () => {
+    await frisby
+      .post(`${url}/todo`, { task: 'clean the kitchen', status: 'pendent' })
+      .expect('status', 201)
+      .then((response => id = response.json.id));
+
+    await frisby
+      .put(`${url}/todo/${id}`, { status: 'pendent' })
+      .expect('status', 400)
+      .then((response) => expect(response.json.message).toBe(invalidEntries))
+  });
+
+  it('You cannot update a task without the "status" field', async () => {
+    await frisby
+      .post(`${url}/todo`, { task: 'clean the kitchen', status: 'pendent' })
+      .expect('status', 201)
+      .then((response => id = response.json.id));
+
+    await frisby
+      .put(`${url}/todo/${id}`, { task: 'clean the kitchen' })
+      .expect('status', 400)
+      .then((response) => expect(response.json.message).toBe(invalidEntries))
+  });
+
+  it('You cannot update a task with the "tasks" field empty', async () => {
+    await frisby
+      .post(`${url}/todo`, { task: 'clean the kitchen', status: 'pendent' })
+      .expect('status', 201)
+      .then((response => id = response.json.id));
+
+    await frisby
+      .put(`${url}/todo/${id}`, { task: '', status: 'pendent' })
+      .expect('status', 400)
+      .then((response) => expect(response.json.message).toBe(invalidEntries))
+  });
+
+  it('You cannot update a task with the "status" field empty', async () => {
+    await frisby
+      .post(`${url}/todo`, { task: 'clean the kitchen', status: 'pendent' })
+      .expect('status', 201)
+      .then((response => id = response.json.id));
+
+    await frisby
+      .put(`${url}/todo/${id}`, { task: 'clean the kitchen', status: '' })
+      .expect('status', 400)
+      .then((response) => expect(response.json.message).toBe(invalidEntries))
+  });
+
+  it('You can update a task successfully', async () => {
+    await frisby
+      .post(`${url}/todo`, { task: 'clean the kitchen', status: 'pendent' })
+      .expect('status', 201)
+      .then((response => id = response.json.id));
+
+    await frisby
+      .put(`${url}/todo/${id}`, { task: 'clean the kitchen', status: 'completed' })
+      .expect('status', 200)
+      .then((response) => {
+        const { id: taskId, task, status } = response.json;
+        expect(taskId).toEqual(id);
+        expect(task).toEqual('clean the kitchen');
+        expect(status).toEqual('completed');
+      })
+  })
+});
